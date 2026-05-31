5219356bef160fc0dbbbd388c0150b182cd8536a
George Karafotias <40838327+george-karafotias@users.noreply.github.com>
2026-05-31 09:43:22 +0300

    Enabling swagger even in prod mode

--- Data.Api/Program.cs
+++ Data.Api/Program.cs
@@ -11,11 +11,11 @@ builder.Services.AddScoped<IAddressParser, AddressParser>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.MapGet("/", () => Results.Ok(new
 {
